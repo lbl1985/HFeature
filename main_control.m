d732762a64673c9b65a3017f770e50bf3d9774b3
Binlong Li <herbert19lee@gmail.com>
2011-08-09 10:22:00 -0400

    automatically find the myfolder in main_control.m function

--- main_control.m
+++ main_control.m
@@ -1,2 +1,4 @@
-myfolder = '/Users/herbert19lee/Documents/MATLAB/work/HFeature';
+% myfolder = '/Users/herbert19lee/Documents/MATLAB/work/HFeature';
+myfolder = which('main_control.m');
+myfolder = myfolder(1:strfind(myfolder, 'main_control.m') - 1);
 master_train(fullfile(myfolder, 'AVIClips05/'), fullfile(myfolder, '10digging'));
