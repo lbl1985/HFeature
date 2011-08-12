219be33b019ae52694236f1ccb1e6e90b2653bb6
Binlong Li <herbert19lee@gmail.com>
2011-08-12 12:07:48 -0400

    simplify the function:subspaceVis

--- 90support/Visualization/subspaceVis.m
+++ 90support/Visualization/subspaceVis.m
@@ -1,16 +1,24 @@
 function subspaceVis(isanetwork)
-
-nFeature = size(isanetwork{1}.W, 1);
 fovea.spatio = isanetwork{1}.spatial_size;
 fovea.temporal = isanetwork{1}.temporal_size;
-[nRow nCol] = plotPre(fovea);
 
+visRealWork(isanetwork{1}.W, fovea);
+% visRealWork(isanetwork{1}.V, fovea);
+end
+
+function visRealWork(data, fovea)
+nFeature = size(data, 1);
+subplotVis(data, nFeature, fovea);
+end
+
+function subplotVis(data, nFeature, fovea)
+[nRow nCol] = plotPre(fovea);
 for i = 1 : nFeature
-    tempW = isanetwork{1}.W(i, :);
-    tempW = reshape(tempW, [fovea.spatio fovea.spatio fovea.temporal]);    
-    figure(i); figure('Name', ['subspace ' num2str(i)]);
+    tempData = data(i, :);
+    tempData = reshape(tempData, [fovea.spatio fovea.spatio fovea.temporal]);    
+    h = figure(1); set(h, 'Name', ['subspace ' num2str(i)]);
     for j = 1 : fovea.temporal
-        subplot(nRow, nCol, j); imshow(tempW(:, :, j));
+        subplot(nRow, nCol, j); imshow(tempData(:, :, j));
         title(['time ' num2str(j)]);
     end    
 end
