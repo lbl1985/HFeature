299d15cb19b900bfa5fffb177bda400036d3927c
Binlong Li <herbert19lee@gmail.com>
2011-08-12 12:17:53 -0400

    plug in the videoSaver class

--- 90support/Visualization/subspaceVis.m
+++ 90support/Visualization/subspaceVis.m
@@ -1,18 +1,21 @@
-function subspaceVis(isanetwork)
+function subspaceVis(isanetwork, isRecord)
 fovea.spatio = isanetwork{1}.spatial_size;
 fovea.temporal = isanetwork{1}.temporal_size;
 
-visRealWork(isanetwork{1}.W, fovea);
+visRealWork(isanetwork{1}.W, fovea, isRecord);
 % visRealWork(isanetwork{1}.V, fovea);
 end
 
-function visRealWork(data, fovea)
+function visRealWork(data, fovea, isRecord)
 nFeature = size(data, 1);
-subplotVis(data, nFeature, fovea);
+subplotVis(data, nFeature, fovea, isRecord);
 end
 
-function subplotVis(data, nFeature, fovea)
+function subplotVis(data, nFeature, fovea, isRecord)
 [nRow nCol] = plotPre(fovea);
+if isRecord
+    v1 = videoSaver('trial1.avi', 11);
+end
 for i = 1 : nFeature
     tempData = data(i, :);
     tempData = reshape(tempData, [fovea.spatio fovea.spatio fovea.temporal]);    
@@ -20,7 +23,14 @@ for i = 1 : nFeature
     for j = 1 : fovea.temporal
         subplot(nRow, nCol, j); imshow(tempData(:, :, j));
         title(['time ' num2str(j)]);
+    end   
+    if isRecord
+        v1.fig = h;
+        v1.saveCore();
     end    
+end
+if isRecord
+    v1.saveClose();
 end
 end
 
