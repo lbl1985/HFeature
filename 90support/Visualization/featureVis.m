252718522ceb7d7d89bf2d6a28844f7b814f228e
Binlong Li <herbert19lee@gmail.com>
2011-08-16 22:59:25 -0400

    loadclip is not suitable to my situation. Create new function cutFrameBySpatialSize. Still under construction. Instead of using tmpMovieVar I am using tmpMovieObj by using video.videoVar class. Going to transfer videoVar from handle into Value

--- 90support/Visualization/featureVis.m
+++ 90support/Visualization/featureVis.m
@@ -1,11 +1,11 @@
-% function featureVis
+function featureVis
 baseFolder = getProjectBaseFolder();
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'all_train_files.mat'));
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_indices.mat'));
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_label_all.mat'));
 load(fullfile(baseFolder, '10digging', 'bases', 'isa2layer_16t20_ts10t14_nf200_gs2_st4t4_l1_isa1layer_16_10_300_1'));
 
-fovea.spatialsize = isanetwork{1}.spatial_size;
+fovea.spatial_size = isanetwork{1}.spatial_size;
 fovea.temporal_size = isanetwork{1}.temporal_size;
 params.avipath = fullfile(baseFolder, 'AVIClips05/');
 all_train_files = all_train_files(1 : length(train_indices));
@@ -16,10 +16,18 @@ for i = 1 : nFiles
     if ~exist(tmpMovieName, 'file')
         error('the file %s is not available', tmpMovieName);
     end
-    tmpMovieVar = loadclip_3dm(tmpMovieName, fovea.spatial_size, 0, 0); 
+    tmpMovieVar = movie2var(tmpMovieName, 0, 1);
+    tmpMovieObj = video.videoVar(tmpMovieVar);
+    tmpMovieObj = cutFrameBySpatialSize(tmpMovieVar, fovea.spatial_size);
+%     tmpMovieVar = loadclip_3dm(tmpMovieName, fovea.spatial_size, 0, 0); 
     coverFlowObj = coverFlow(tmpMovieVar);
     coverFlowObj = coverFlowObj.setFrameRangeAll;
     coverFlowObj.playConsecutiveCoverFlow;
     clear coverFlowObj
 %     coverFlowObj1.
 end
+end
+
+function outputVideo = cutFrameBySpatialSize(inputVideo, spatial_size)
+siz = size(inputVideo);
+nframe = 
