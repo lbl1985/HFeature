8fb88d25c4c8376bb84ade51d0bb73d6672a6e86
Binlong Li <herbert19lee@gmail.com>
2011-08-16 23:31:49 -0400

    feature Vis modification for cropVideo should be almost done. Stage: before debug

--- 90support/Visualization/featureVis.m
+++ 90support/Visualization/featureVis.m
@@ -5,10 +5,10 @@ load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_indices.mat')
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_label_all.mat'));
 load(fullfile(baseFolder, '10digging', 'bases', 'isa2layer_16t20_ts10t14_nf200_gs2_st4t4_l1_isa1layer_16_10_300_1'));
 
-fovea.spatial_size = isanetwork{1}.spatial_size;
+fovea.spatial_size = isanetwork{1}.spatial_size; %#ok<USENS>
 fovea.temporal_size = isanetwork{1}.temporal_size;
 params.avipath = fullfile(baseFolder, 'AVIClips05/');
-all_train_files = all_train_files(1 : length(train_indices));
+all_train_files = all_train_files(1 : length(train_indices)); %#ok<NODEF>
 
 nFiles = length(all_train_files);
 for i = 1 : nFiles
@@ -17,17 +17,13 @@ for i = 1 : nFiles
         error('the file %s is not available', tmpMovieName);
     end
     tmpMovieVar = movie2var(tmpMovieName, 0, 1);
-    tmpMovieObj = video.videoVar(tmpMovieVar);
-    tmpMovieObj = cutFrameBySpatialSize(tmpMovieObj, fovea.spatial_size);
-%     tmpMovieVar = loadclip_3dm(tmpMovieName, fovea.spatial_size, 0, 0); 
+    tmpMovieObj = video.croppedVideoVar(tmpMovieVar, fovea.spatial_size);
+    tmpMovieObj.cropVideoForFeatureDetection();
+    
     coverFlowObj = coverFlow(tmpMovieObj);
     coverFlowObj = coverFlowObj.setFrameRangeAll;
     coverFlowObj.playConsecutiveCoverFlow;
     clear coverFlowObj
 %     coverFlowObj1.
 end
-end
-
-function outputVideo = cutFrameBySpatialSize(inputVideo, spatial_size)
-
 end
