a1e6160859be42e656b98f6809cc9fbbc6a9e631
Binlong Li <herbert19lee@gmail.com>
2011-08-16 22:51:43 -0400

    instead of movie2var.m read movie file by using loadclip_3dm. This function with additional ability to modifiy the video according to the coresponding spatial size

--- 90support/Visualization/featureVis.m
+++ 90support/Visualization/featureVis.m
@@ -3,9 +3,12 @@ baseFolder = getProjectBaseFolder();
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'all_train_files.mat'));
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_indices.mat'));
 load(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_label_all.mat'));
+load(fullfile(baseFolder, '10digging', 'bases', 'isa2layer_16t20_ts10t14_nf200_gs2_st4t4_l1_isa1layer_16_10_300_1'));
 
-all_train_files = all_train_files(1 : length(train_indices));
+fovea.spatialsize = isanetwork{1}.spatial_size;
+fovea.temporal_size = isanetwork{1}.temporal_size;
 params.avipath = fullfile(baseFolder, 'AVIClips05/');
+all_train_files = all_train_files(1 : length(train_indices));
 
 nFiles = length(all_train_files);
 for i = 1 : nFiles
@@ -13,7 +16,7 @@ for i = 1 : nFiles
     if ~exist(tmpMovieName, 'file')
         error('the file %s is not available', tmpMovieName);
     end
-    tmpMovieVar = movie2var(tmpMovieName, 0, 1);
+    tmpMovieVar = loadclip_3dm(tmpMovieName, fovea.spatial_size, 0, 0); 
     coverFlowObj = coverFlow(tmpMovieVar);
     coverFlowObj = coverFlowObj.setFrameRangeAll;
     coverFlowObj.playConsecutiveCoverFlow;
