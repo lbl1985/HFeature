d975b5f229258baf6296f2930f08f7bebfb60157
Binlong Li <herbert19lee@gmail.com>
2011-08-17 16:29:14 -0400

    first version of calculating feature location is done.

---
+++ 90support/Visualization/calculateFeatureLocations.m
@@ -0,0 +1,23 @@
+function calculateFeatureLocations(tmpMovieObj, ClassLabels)
+% croppedVideoObj
+
+displayOffsite = 5;
+
+numFeaturePerRow = tmpMovieObj.siz(1) / tmpMovieObj.spatial_size;
+numFeaturePerFrame = tmpMovieObj.siz(1) * tmpMovieObj.siz(2) / (tmpMovieObj.spatial_size^2);
+numOfSlides = tmpMovieObj.siz(end) / tmpMovieObj.temporal_size;
+nFeature = numFeaturePerFrame * numOfSlides;
+
+assert(nFeature == length(ClassLabels), 'nFeature Calculated is not equal to Experimental Results');
+
+for i = 1 : nFeature
+    slideIndex = mod(i / numFeaturePerFrame);
+    rowIndex = mod(slideIndex / numFeaturePerRow);
+    colIndex = ceil(slideIndex / numFeaturePerRow);
+    
+    textLocation = zeros(tmpMovieObj.temporal_size, 3);
+    textLocation(:, 1) = (rowIndex - 1) * tmpMovieObj.spatial_size + displayOffsite;
+    textLocation(:, 2) = (colIndex - 1) * tmpMovieObj.spatial_size + displayOffsite;
+    textLocation(:, 3) = slideIndex * (1 : tmpMovieObj.temporal_size)';
+end
+    
