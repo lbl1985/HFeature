0e296bf2b14f2ae07a2186931ea7e794693b9923
Binlong Li <herbert19lee@gmail.com>
2011-08-17 16:00:23 -0400

    debug the croppedVideoVar for time Crop. Now this functionality can work properly

--- 90support/+video/@croppedVideoVar/croppedVideoVar.m
+++ 90support/+video/@croppedVideoVar/croppedVideoVar.m
@@ -22,15 +22,16 @@ classdef croppedVideoVar < video.videoVar
     
     methods
         function cropVideoForFeatureDetection(obj)
+            obj = obj.cropVideoForFeatureDetectionInTime();
             if obj.ndim == 3
                 % do operation for gray images
-                for t = 1 : obj.corppedSiz(end)
+                for t = 1 : obj.croppedSiz(end)
                     obj.croppedVideo(:, :, t) = obj.Data(1 : floor(obj.siz(1)/obj.spatial_size) * obj.spatial_size, ...
                         1 : floor(obj.siz(2) / obj.spatial_size) * obj.spatial_size, t);
                 end
             elseif obj.ndim == 4
                 % do operation for color images
-                for t = 1 : obj.corppedSiz(end)
+                for t = 1 : obj.croppedSiz(end)
                     obj.croppedVideo(:, :, :, t) = obj.Data(1 : floor(obj.siz(1)/obj.spatial_size) * obj.spatial_size, ...
                         1 : floor(obj.siz(2) / obj.spatial_size) * obj.spatial_size, :, t);
                 end
@@ -40,7 +41,7 @@ classdef croppedVideoVar < video.videoVar
     end
     
     methods % Utility Function
-        function cropVideoForFeatureDetectionInTime(obj)
+        function obj = cropVideoForFeatureDetectionInTime(obj)
             obj.croppedSiz(end) = floor(obj.nFrame/obj.temporal_size) * obj.temporal_size;
             if obj.ndim == 3
                 obj.croppedVideo = obj.croppedVideo(:, :, 1 : obj.croppedSiz(end));
