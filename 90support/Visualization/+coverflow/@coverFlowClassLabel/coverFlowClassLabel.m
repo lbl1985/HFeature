6481a04762da4439860ae26180e7125f18f21ccb
Binlong Li <herbert19lee@gmail.com>
2011-08-18 09:24:08 -0400

    coverFlowClassLabel class first draft. overwrite function playConsecutiveCoverFlow implement engraveText (not sure whether it will work or not). Regenerate function calculateFeatureLocation -- based on one feature index only -- instead of batch location calculation

--- 90support/Visualization/+coverflow/@coverFlowClassLabel/coverFlowClassLabel.m
+++ 90support/Visualization/+coverflow/@coverFlowClassLabel/coverFlowClassLabel.m
@@ -15,6 +15,8 @@ classdef coverFlowClassLabel < coverFlowOrig
     
     methods     %initialization function
         function obj = coverFlowClassLabel(croppedVideoObj, inputClassLabels)
+            assert(isobject(croppedVideoObj), 'croppedVideoObj has to be object');
+            
             obj = obj@coverFlowOrig(croppedVideoObj);
             obj.classLabel = inputClassLabels;
             
@@ -25,12 +27,48 @@ classdef coverFlowClassLabel < coverFlowOrig
             
             assert(obj.nFeature == length(obj.classLabels), ...
                 'nFeature Calculated is not equal to Experimental Results');
+            
+            obj.frameRange = 1 : 10;
         end        
     end
     
     methods     % Ability Functions
-        function plotClassifiedLabel(obj)
-            for i = 1 : obj.data.
+        function [w h] = playConsecutiveCoverFlow(obj)
+            obj.figHandel = figure();
+            % frameRange is the showing frames indicator.
+            for i = obj.frameRange(1) : obj.frameRange(end - obj.param.stackSize + 1)
+                obj = obj.setFrames(obj.frameRange(i) : obj.frameRange(i + obj.param.stackSize -1));
+                if isobject(obj.data)
+                    [w h] = obj.coverFlowCore(obj.data.Data);
+                else
+                    [w h] = obj.coverFlowCore(obj.data);
+                end
+                pause(1/11);
+                obj.engraveText(h);
+            end
+        end
+        
+        function engraveText(obj, h)
+            for i = 1 : obj.param.stackSize
+                figure(h(i));   t = obj.frames(i);
+                slideIndex = floor(t/ obj.data.temporal_size); 
+                featureIndex = slideIndex * obj.numFeaturePerFrame + 1 : ...
+                    (slideIndex + 1) * obj.numFeaturePerFarme;
+                for j = featureIndex(1) : featureIndex(end)
+                    location = obj.calculateFeatureLocation(j);
+                    text(location(1), location(2), num2str(obj.classLabels(j)));
+                end
+            end                
+        end
+        
+        function textLocation = calculateFeatureLocation(obj, i)
+            slideIndex = mod(i / obj.numFeaturePerFrame);
+            rowIndex = mod(slideIndex / obj.numFeaturePerRow);
+            colIndex = ceil(slideIndex / obj.numFeaturePerRow);
+
+            textLocation = zeros(1, 2);
+            textLocation(1) = (rowIndex - 1) * obj.data.spatial_size + obj.displayOffsite;
+            textLocation(2) = (colIndex - 1) * obj.data.spatial_size + obj.displayOffsite;
         end
     end
     
