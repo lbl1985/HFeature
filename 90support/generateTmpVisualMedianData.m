22d08d14b02becda680aa0ffc9266832609a95eb
Binlong Li <herbert19lee@gmail.com>
2011-08-22 11:39:31 -0400

    support function for extract the necessary section of correspoinding required videos

---
+++ 90support/generateTmpVisualMedianData.m
@@ -0,0 +1,22 @@
+function generateTmpVisualMedianData(idx)
+% This function is designed for prepare for the visualization purpose.
+% Save the information sections corresponding to the specific required
+% videos.
+
+% idx = 1 : 2;
+
+baseFolder = getProjectBaseFolder();
+load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'all_train_files.mat'));
+load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'save_train_indices.mat'));
+load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'train_label_all.mat'));
+
+all_train_files_backup = all_train_files;
+all_train_files = all_train_files(idx);
+save(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'all_train_files.mat'), 'all_train_files');
+
+train_indices_backup = train_indices;
+train_indices = train_indices{1}(idx);
+save(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_indices.mat'), 'train_indices');
+
+save(fullfile(baseFolder, 'Results', 'tmpVisualMedianData', 'train_label_all.mat'), 'train_label_all');
+
