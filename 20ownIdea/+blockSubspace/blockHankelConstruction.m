9553f3764139d4862cd8d5e9e85a89fb9fc47452
Binlong Li <herbert19lee@gmail.com>
2011-08-24 08:44:52 -0400

    if numSlide is less or equal to hankelWindowSize. This video will not do the blockHankelConstruction. Instead it will returen the number of featurePerFrame empty cells. The reason still return cells is that we need to keep consistence with train_indices

--- 20ownIdea/+blockSubspace/blockHankelConstruction.m
+++ 20ownIdea/+blockSubspace/blockHankelConstruction.m
@@ -1,18 +1,30 @@
 function HankelVideo = blockHankelConstruction(hankelWindowSize, featureMatrix, featureIndexObj)
-% Since the default featureMatrix is row orientated, each row is a feature
-featureMatrix = featureMatrix';
-assert(isequal(size(featureMatrix, 2), featureIndexObj.nFeature), ...
-    'number of features is not identical between calculation and experiments');
+    % Since the default featureMatrix is row orientated, each row is a feature
+    featureMatrix = featureMatrix';
+    assert(isequal(size(featureMatrix, 2), featureIndexObj.nFeature), ...
+        'number of features is not identical between calculation and experiments');
 
-% each video should contains # of featuresPerFrame Hankels. 
-% Each Hankel represents the dynamics alonging the time through each block
-% in spatial dimension
-HankelVideo = cell(featureIndexObj.numFeaturePerFrame, 1);
-
-for i = 1 : featureIndexObj.numFeaturePerFrame
-    tmpFeatureIndexExtracting = i : featureIndexObj.numFeaturePerFrame : ...
-        featureIndexObj.nFeature;
-    tmpFF = featureMatrix(:, tmpFeatureIndexExtracting);
-    HankelVideo{i} = hankelConstruction(tmpFF, hankelWindowSize);
+    % each video should contains # of featuresPerFrame Hankels. 
+    % Each Hankel represents the dynamics alonging the time through each block
+    % in spatial dimension
+    HankelVideo = cell(featureIndexObj.numFeaturePerFrame, 1);
+    isExtractable = checkIsNumOfSlideLargerThanHankelWindowSize(hankelWindowSize, featureIndexObj);
+    if isExtractable
+        for i = 1 : featureIndexObj.numFeaturePerFrame
+            tmpFeatureIndexExtracting = i : featureIndexObj.numFeaturePerFrame : ...
+                featureIndexObj.nFeature;
+            tmpFF = featureMatrix(:, tmpFeatureIndexExtracting);
+            HankelVideo{i} = hankelConstruction(tmpFF, hankelWindowSize);
+        end
+    end
 end
 
+function isExtractable = checkIsNumOfSlideLargerThanHankelWindowSize(...
+    hankelWindowSize, featureIndexObj)
+    numSlide = featureIndexObj.nFeature / featureIndexObj.numFeaturePerFrame;
+    if numSlide > hankelWindowSize
+        isExtractable = 1;
+    else
+        isExtractable = 0;
+    end
+end
