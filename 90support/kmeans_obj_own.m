9bf555656a793ac5a709ecc621734949b053ab9d
Binlong Li <herbert19lee@gmail.com>
2011-08-24 09:31:31 -0400

    since my feature is already column wised. I do not need to transpose my matrix at the beginning of the function.

--- 90support/kmeans_obj_own.m
+++ 90support/kmeans_obj_own.m
@@ -31,7 +31,6 @@
 % Modified superficially by Adam Coates, 6/1/10
 function [label,center, obj] = kmeans_obj_own(X, k)
 
-X=X';
 n = size(X,2);
 last = 0;
 label = ceil(k*rand(1,n));  % random initialization
