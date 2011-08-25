5605f5f0c9cbab4828005ddf3a5db1f9ad78da85
Binlong Li <herbert19lee@gmail.com>
2011-08-25 08:43:10 -0400

    block Dynamics starting

---
+++ 20ownIdea/blockDynamics/main_blockDynamics.m
@@ -0,0 +1,5 @@
+baseFolder = getProjectBaseFolder();
+load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'all_train_files.mat'));
+load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'save_train_indices.mat'));
+load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'train_label_all.mat'));
+load(fullfile(baseFolder, '10digging', 'bases', 'isa2layer_16t20_ts10t14_nf200_gs2_st4t4_l1_isa1layer_16_10_300_1'));
