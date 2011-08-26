c4fb794a710840d3a6ce0ef7d0c6ab1548d816c0
Binlong Li <herbert19lee@gmail.com>
2011-08-26 08:53:19 -0400

    main frame for frame-feature index correspondence.

--- 20ownIdea/blockDynamics/main_blockDynamics.m
+++ 20ownIdea/blockDynamics/main_blockDynamics.m
@@ -1,11 +1,36 @@
 clear
 baseFolder = getProjectBaseFolder();
-load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'all_train_files.mat'));
-load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'save_train_indices.mat'));
-load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'train_label_all.mat'));
-load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'trainKmeans_excpt_Xtrain_raw.mat'));
-load(fullfile(baseFolder, '10digging', 'bases', 'isa2layer_16t20_ts10t14_nf200_gs2_st4t4_l1_isa1layer_16_10_300_1'));
+datasetName = 'hw2';
+switch datasetName
+    case 'hw2'
+        % This is using HW2 training data
+        load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'all_train_files.mat'));
+        load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'save_train_indices.mat'));
+        load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'train_label_all.mat'));
+        load(fullfile(baseFolder, 'Results', 'VisualMedianData', 'trainKmeans_excpt_Xtrain_raw.mat'));
+        load(fullfile(baseFolder, '10digging', 'bases', 'isa2layer_16t20_ts10t14_nf200_gs2_st4t4_l1_isa1layer_16_10_300_1'));
+    case 'kth'
+end
 
 fovea.spatial_size = isanetwork{1}.spatial_size; 
 fovea.temporal_size = isanetwork{1}.temporal_size;
 
+featureOrderFileIndex = featureIndex2FileIndex(train_indices{1});
+numWord = max(train_label_all{1}{1});
+
+for wordId = 25
+    index = find(train_label_all{1}{1} == wordId);
+    featureIndexForVideo = getFeatureIndexForVideo(index, train_label_all{1});
+    videoPatchAll = [];
+    while i ~= size(featureIndexForVideo, 1)
+%     for i = 1 : size(featureIndexForVideo, 1)
+        M = loadingData(datasetName, folder, all_train_files{i});
+        videoSample = transact_dense_samp_raw(M, fovea, params);
+        sameTypeIndexInSameVideo = find(featureIndexForVideo(:, 1) == featureIndexForVideo(i));
+        for j = 1 : length(sameTypeIndexInSameVideo)
+            videoPatch = cat(1, videoPatch, {reshape(videoSample, [fovea.spatial_size ...
+                fovea.spatial_size fovea.temporal_size])});
+        end
+        i = j;        
+    end
+end
