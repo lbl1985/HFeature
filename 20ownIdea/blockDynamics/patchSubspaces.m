418c44845f6ba01d7435593e8e7f7d5723e22c9e
Binlong Li <herbert19lee@gmail.com>
2011-08-30 15:54:49 -0400

    clean up patchSubspaces.m function.

--- 20ownIdea/blockDynamics/patchSubspaces.m
+++ 20ownIdea/blockDynamics/patchSubspaces.m
@@ -1,17 +1,14 @@
-function patchSubspaces(wordId, params_subspace, dataSavingFolder)
+function [wordHankelSubspaces hankelWindowSizeBatch spaceAngle] = ...
+    patchSubspaces(wordId, params_subspace, dataSavingFolder)
 
 load(['videoPatchAll_Word' num2str(wordId) '.mat']);
 
 if nargin > 1
     fovea = params_subspace.fovea;
-%     subspaceDim_pca = params_subspace.subspaceDim_pca;
     subspaceDim_hankel = params_subspace.subspaceDim_hankel;
-%     hankelWindowSize = params_subspace.hankelWindowSize;
 else
     fovea.spatial_size = 20; fovea.temporal_size = 14;
-%     subspaceDim_pca = 3;
     subspaceDim_hankel = 1;
-%     hankelWindowSize = 4;
 end
 
 nPatches = length(wordPatches);
@@ -48,14 +45,6 @@ for i = 1 : nPatches
     wordHankelSubspaces{i} = U(:, 1 : subspaceDim_hankel);
     spaceAngle(i) = subspace(wordHankelSubspaces{1}, wordHankelSubspaces{i});
 end
-
-for i = 1 : nPatches
-    spaceAngle(i) = subspace(wordHankelSubspaces{1}, wordHankelSubspaces{i});
-end
-
-% wordHankelSubspacesArray = assembleCellData2Array(wordHankelSubspaces, 2);
-% spaceAngle = acos(abs(wordHankelSubspacesArray(:, 1 : subspaceDim_hankel)' * ...
-%     wordHankelSubspacesArray));
 
 bins = 0 : pi/ 100 : pi;
 h = figure(1); hist(spaceAngle, bins);
