c2eab902c117d9bd16921a936949775c1e715a4c
Binlong Li <herbert19lee@gmail.com>
2011-09-09 11:54:32 -0400

    can handle KTH for .mat situation. which means we don not need to read video any more. Just load the data. With consistent interfece: variable tmpMovieVar.

--- 20ownIdea/+blockSubspace/getFeatureIndexObj.m
+++ 20ownIdea/+blockSubspace/getFeatureIndexObj.m
@@ -1,10 +1,15 @@
 function featureIndexObj = getFeatureIndexObj(i, all_train_files, params)
     fovea = params.fovea;
-    tmpMovieName = fullfile(params.avipath, [all_train_files{i} '.avi']);
-    if ~exist(tmpMovieName, 'file')
-        error('the file %s is not available', tmpMovieName);
+    if ~strcmp(all_train_files{i}(end - 4 : end), '.mat')
+        tmpMovieName = fullfile(params.avipath, [all_train_files{i} '.avi']);
+        if ~exist(tmpMovieName, 'file')
+            error('the file %s is not available', tmpMovieName);
+        end
+        tmpMovieVar = movie2var(tmpMovieName, 0, 1);
+    else
+        LOAD = load(fullfile(params.avipath, all_train_files{i}));
+        tmpMovieVar = LOAD.I;
     end
-    tmpMovieVar = movie2var(tmpMovieName, 0, 1);
     
     tmpMovieObj = video.croppedVideoVar(tmpMovieVar, fovea);
     tmpMovieSizeObj = tmpMovieObj.calculateSizeOfFeatureDetection();
