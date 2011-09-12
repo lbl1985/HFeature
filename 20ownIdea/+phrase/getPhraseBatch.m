b1fbc8201beb13b8f84411e2325b2da118174dd3
Binlong Li <herbert19lee@gmail.com>
2011-09-12 11:40:09 -0400

    getPhraseBatch now support multiple kmeans for initial word.

--- 20ownIdea/+phrase/getPhraseBatch.m
+++ 20ownIdea/+phrase/getPhraseBatch.m
@@ -4,18 +4,27 @@ function [labelPhraseAll train_indices] =  getPhraseBatch(train_indices, ...
 
 all_train_files = all_train_files(1 : length(train_indices)); 
 
-labelPhraseAll = [];
+% labelPhraseAll = [];
+
+nKmeans = length(train_label_all);
+labelPhraseAll = cell(1, nKmeans);
 
 nFiles = length(all_train_files);
 for i = 1 : nFiles
     featureIndexObj = blockSubspace.getFeatureIndexObj(i, all_train_files, params);
     
-    tmpFeatureMatrix = blockSubspace.getFeatureMatrix(i, train_indices, train_label_all{1}{1});
+    for j = 1 : nKmeans
+        tmpFeatureMatrix = blockSubspace.getFeatureMatrix(i, train_indices, train_label_all{1}{j});
     
-    tmpPhraseEachVideo = phrase.getPhraseEachVideo(tmpFeatureMatrix, featureIndexObj, 2);
-    train_indices{i}.blockPhraseIndex.start = size(labelPhraseAll, 1) + 1; %#ok<*SAGROW>
-    labelPhraseAll = cat(1, labelPhraseAll, tmpPhraseEachVideo);
-    train_indices{i}.blockPhraseIndex.end   = size(labelPhraseAll, 1);
+        tmpPhraseEachVideo = phrase.getPhraseEachVideo(tmpFeatureMatrix, featureIndexObj, 2);
+        if j == 1
+            train_indices{i}.blockPhraseIndex.start = size(labelPhraseAll{j}, 1) + 1; %#ok<*SAGROW>
+        end
+        labelPhraseAll{j} = cat(1, labelPhraseAll{j}, tmpPhraseEachVideo);
+        if j == 1
+            train_indices{i}.blockPhraseIndex.end   = size(labelPhraseAll{j}, 1);
+        end
+    end
     
     writenum(i);
 end
